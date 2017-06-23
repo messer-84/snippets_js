e85c805e40d06970a8a0148f59b2d9e402977c17
Maksim <gtl.vorobyov@gmail.com>
2017-06-23 12:06:01 +0300

    f

---
+++ conditionForSlick.js
@@ -0,0 +1,6 @@
+      if (Math.abs(nextSlide - currentSlide) == 1) {
+        directionSlide = (nextSlide - currentSlide > 0) ? "right" : "left";
+      }
+      else {
+        directionSlide = (nextSlide - currentSlide > 0) ? "left" : "right";
+      }
